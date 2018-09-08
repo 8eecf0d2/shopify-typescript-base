7a36d3a3c00c49f2b3dbc945ce927a8f0d51f786
Brod <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-09-08 18:48:30 +1000

    :fallen_leaf: removed uglify, bundle analyzer and unused @types

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -1,19 +1,10 @@
 const path = require("path");
 const webpack = require("webpack");
-const uglifyJSPlugin = require("uglifyjs-webpack-plugin");
-const bundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: { bundle: "./src/client/ts/index.ts" },
   devtool: "source-map",
   mode: process.env.MODE || process.env.NODE_ENV,
-  plugins: [
-    new bundleAnalyzerPlugin({
-      analyzerMode: "static",
-      openAnalyzer: false,
-      reportFilename: "./dist/webpack-report.html",
-    }),
-  ],
   module: {
     rules: [
       {
@@ -21,16 +12,6 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
-    ]
-  },
-  optimization: {
-    minimizer: [
-      new uglifyJSPlugin({
-        sourceMap: true,
-        uglifyOptions: {
-          output: { ascii_only: true }
-        }
-      })
     ]
   },
   resolve: {
