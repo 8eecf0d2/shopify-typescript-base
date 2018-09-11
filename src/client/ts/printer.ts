998d758dd3c9ff2d5add820c430cc44e6f29e256
Brod <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-09-12 07:38:32 +1000

    :sparkles: added more template variables

--- src/client/ts/printer.ts
+++ src/client/ts/printer.ts
@@ -1,13 +1,33 @@
 //@ts-ignore
 import * as Liquid from "liquidjs";
+import { resource } from "./resource";
 const liquid = Liquid();
 
 import { OrderSchema, TemplateSchema } from "../../shared/ts/shcema";
 
 export class Printer {
   static async print (template: TemplateSchema.Object, order: OrderSchema.Object): Promise<string> {
-    const html = await liquid.parseAndRender(template.content, { order: order });
+    const html = await liquid.parseAndRender(template.content, await Printer.variables(order.id));
 
     return html;
+  }
+
+  static async variables (order?: string): Promise<Printer.Variables> {
+    const requests = {
+      shop: await resource.shopify.handler({ method: "GET", path: `/admin/shop.json` }),
+      order: !order ? { data: { order: OrderSchema.empty() } } : await resource.shopify.handler({ method: "GET", path: `/admin/orders/${order}.json` }),
+    }
+
+    return {
+      shop: requests.shop.data.shop,
+      order: OrderSchema.parse(requests.order.data.order)[0],
+    }
+  }
+}
+
+export namespace Printer {
+  export interface Variables {
+    shop: any;
+    order: OrderSchema.Object;
   }
 }
