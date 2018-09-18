378540cd4cc990ea65213e997d652381c95c6d65
Brod <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-09-19 07:16:35 +1000

    :pencil: typo

--- src/shared/ts/database.ts
+++ src/shared/ts/database.ts
@@ -4,11 +4,6 @@ import * as models from "./model";
 export class Database {
 
   constructor() {
-    console.log({
-      accessKeyId: process.env.AWS_IAM_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_IAM_SECRET_KEY,
-      region: process.env.AWS_REGION,
-    });
     dynamoose.AWS.config.update({
       accessKeyId: process.env.AWS_IAM_ACCESS_KEY,
       secretAccessKey: process.env.AWS_IAM_SECRET_KEY,
